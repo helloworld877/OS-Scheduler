0be15d7740347ae992ab5070b432cdb6d9d61f2f
helloworld877 <mmghaly3@gmail.com>
2022-12-16 17:59:34 +0200

    process now runs and pauses

--- process.c
+++ process.c
@@ -1,27 +1,37 @@
 #include "headers.h"
 
-
 /* Modify this file as needed*/
 int remainingtime;
 int msgq_id;
 
+int SIGUSR1_handler(int signum);
+int SIGUSR2_handler(int signum);
 int main(int agrc, char *argv[])
 {
-    //initClk();
-    remainingtime=atoi(argv[1]);
-    printf("\n%d\n", remainingtime);
-
-    //TODO The process needs to get the remaining time from somewhere
-    //remainingtime = ??;
-
-    
+    signal(SIGUSR1, SIGUSR1_handler);
+    initClk();
+    remainingtime = atoi(argv[1]);
 
     while (remainingtime > 0)
     {
-        
+        remainingtime -= 1;
+        // assuming that the clock is 1 second
+        sleep(1);
     }
 
-    //destroyClk(false);
+    destroyClk(false);
 
     return 0;
+}
+int SIGUSR1_handler(int signum)
+{
+    sigemptyset(&sigact.sa_mask);
+    sigact.sa_flags = 0;
+    sigact.sa_handler = SIGUSR2_handler;
+    sigaction(SIGUSR1, &sigact, NULL);
+    pause();
+}
+int SIGUSR2_handler(int signum)
+{
+    return;
 }
