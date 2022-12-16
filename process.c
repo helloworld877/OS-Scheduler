b2f2e88b58aec06f197b4ff8619a67324649e5b1
helloworld877 <mmghaly3@gmail.com>
2022-12-16 18:18:46 +0200

    Update process.c

--- process.c
+++ process.c
@@ -1,5 +1,15 @@
 #include "headers.h"
 
+/*
+HOW TO USE PROCESS
+->when u create a process send to its pid SIGUSR1 (PAUSE TILL I TELL YOU)
+->when it is the turn of the process to run send to its pid SIGUSR2 (CONTINUE YOUR PROCESS)
+->if you want to pause te running process send SIGUSR1
+kill(pid_of_the _process,SIGUSR1/2)
+
+
+*/
+
 /* Modify this file as needed*/
 int remainingtime;
 int msgq_id;
@@ -28,7 +38,7 @@ int SIGUSR1_handler(int signum)
     sigemptyset(&sigact.sa_mask);
     sigact.sa_flags = 0;
     sigact.sa_handler = SIGUSR2_handler;
-    sigaction(SIGUSR1, &sigact, NULL);
+    sigaction(SIGUSR2, &sigact, NULL);
     pause();
 }
 int SIGUSR2_handler(int signum)
